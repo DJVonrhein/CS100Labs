8d97f1d4cee9b8cf3e3bca5c59fbfa3993108332
DJVonrhein <dvonr001@ucr.edu>
2020-11-05 13:27:29 -0800

    op.hpp finalized, about to change 4 of it's tests' expected values.

--- op.hpp
+++ op.hpp
@@ -5,9 +5,11 @@
 
 class Op : public Base {
     public:
-        Op(double value) : Base() { }
+        Op(double val) : Base() {value = val; }
         virtual double evaluate() { return value; }
-        virtual std::string stringify() { return "value"; }
+        virtual std::string stringify() { return std::to_string(value); }
+    private:
+	double value;
 };
 
 
@@ -16,27 +18,27 @@ class Op : public Base {
 //MOCK for positive double return val
 class SevenOpMock: public Base {
     public:
-        SevenOpMock() { };
+        SevenOpMock() : Base() { }
 
         virtual double evaluate() { return 7.5; }
-        virtual string stringify() { return "7.5"; }
-}
+        virtual std::string stringify() { return "7.5"; }
+};
 
 //MOCK for negative double return val
 class NegOpMock: public Base {
     public:
-        NegOpMock() { };
+        NegOpMock() : Base() { }
 
         virtual double evaluate() { return -1.0; }
-        virtual string stringify() { return "-1.0"; }
-}
+        virtual std::string stringify() { return "-1.0"; }
+};
 
 //MOCK for zero return val
 class ZeroOpMock: public Base {
     public:
-        ZeroOpMock() { };
+        ZeroOpMock() : Base() { }
 
         virtual double evaluate() { return 0.0; }
-        virtual string stringify() { return "0.0"; }
-}
+        virtual std::string stringify() { return "0.0"; }
+};
 #endif //__OP_HPP__
