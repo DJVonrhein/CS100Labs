011a49085a7e11d70b16c4454983808a651a7599
DJVonrhein <dvonr001@ucr.edu>
2020-11-04 17:44:48 -0800

    Created mocks in op.hpp; deleted an extraneous file

--- op.hpp
+++ op.hpp
@@ -10,4 +10,33 @@ class Op : public Base {
         virtual std::string stringify() { return ""; }
 };
 
+
+
+
+//MOCK for positive double return val
+class SevenOpMock: public Base {
+    public:
+        SevenOpMock() { };
+
+        virtual double evaluate() { return 7.5; }
+        virtual string stringify() { return "7.5"; }
+}
+
+//MOCK for negative double return val
+class NegOpMock: public Base {
+    public:
+        NegOpMock() { };
+
+        virtual double evaluate() { return -1.0; }
+        virtual string stringify() { return "-1.0"; }
+}
+
+//MOCK for zero return val
+class ZeroOpMock: public Base {
+    public:
+        ZeroOpMock() { };
+
+        virtual double evaluate() { return 0.0; }
+        virtual string stringify() { return "0.0"; }
+}
 #endif //__OP_HPP__
