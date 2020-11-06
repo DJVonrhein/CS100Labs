83c100c689a8aa9b52f59a07a1f5adb23b31d3a3
DJVonrhein <dvonr001@ucr.edu>
2020-11-05 17:40:59 -0800

    Made the sub mock tests and cleaned up mock functions.

--- sub.hpp
+++ sub.hpp
@@ -1,12 +1,13 @@
-#ifndef __OP_HPP__
-#define __OP_HPP__
+#ifndef __SUB_HPP__
+#define __SUB_HPP__
 
 #include "base.hpp"
+#include "op.hpp"
 
 class Sub : public Base {
     public:
-        Sub(Base* l, Base* r) : Base() {lhs = l; rhs = r }
-        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate() }
+        Sub(Base* l, Base* r) : Base() {lhs = l; rhs = r; }
+        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate(); }
         virtual std::string stringify() { return (lhs->stringify() + " - " + rhs->stringify()); }
     private:
         Base* lhs;
@@ -20,8 +21,8 @@ class Sub : public Base {
 
 class SubMockNeg : public Base {
     public:
-        SubMockNeg() : Base() {lhs = -4; rhs = -3 }
-        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate() }
+        SubMockNeg() : Base() {lhs = new Op( -4); rhs =new Op(-3); }
+        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate(); }
         virtual std::string stringify() { return (lhs->stringify() + " - " + rhs->stringify()); }
     private:
         Base* lhs;
@@ -30,8 +31,8 @@ class SubMockNeg : public Base {
 
 class SubMockZero : public Base {
     public:
-        SubMockNeg() : Base() {lhs = 0; rhs = -3 }
-        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate() }
+        SubMockZero() : Base() {lhs =new  Op( 0); rhs =new Op(-3); }
+        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate(); }
         virtual std::string stringify() { return (lhs->stringify() + " - " + rhs->stringify()); }
     private:
         Base* lhs;
@@ -40,10 +41,12 @@ class SubMockZero : public Base {
 
 class SubMockDouble : public Base {
     public:
-        SubMockDouble() : Base() {lhs = 3.5; rhs = -4.2 }
-        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate() }
+        SubMockDouble() : Base() {lhs =new Op(3.5); rhs = new Op(-4.2); }
+        virtual double evaluate() { return lhs->evaluate() - rhs->evaluate(); }
         virtual std::string stringify() { return (lhs->stringify() + " - " + rhs->stringify()); }
     private:
         Base* lhs;
         Base* rhs;
 };
+
+#endif
--- sub_test.hpp
+++ sub_test.hpp
@@ -8,32 +8,32 @@
 
 TEST(SubTest, SubMockEvalNeg){
 	SubMockNeg* test = new SubMockNeg(); 
-	EXPECT_EQ(test->evaluate(), x);
+	EXPECT_EQ(test->evaluate(), -1);
 }
 
 TEST(SubTest, SubMockEvalZero){
 	SubMockZero* test = new SubMockZero();
-	EXPECT_EQ(test->evaluate(), x);
+	EXPECT_EQ(test->evaluate(), 3);
 }
 
 TEST(SubTest, SubMockEvalDouble){
 	SubMockDouble* test = new SubMockDouble();
-	EXPECT_EQ(test->evaluate(), x);
+	EXPECT_EQ(test->evaluate(), 7.7);
 }
 
 TEST(SubTest, SubMockStringifyNeg){
 	SubMockNeg* test = new SubMockNeg();
-	EXPECT_EQ(test->evaluate(), x);
+	EXPECT_EQ(test->stringify(), "-4.000000 - -3.000000");
 }
 
 TEST(SubTest, SubMockStringifyZero){
 	SubMockZero* test = new SubMockZero();
-	EXPECT_EQ(test->evaluate(), x);
+	EXPECT_EQ(test->stringify(), "0.000000 - -3.000000");
 }
 
 TEST(SubTest, SubMockStringifyDouble){
 	SubMockDouble* test = new SubMockDouble();
-	EXPECT_EQ(test->evaluate(), x);
+	EXPECT_EQ(test->stringify(), "3.500000 - -4.200000");
 }	
 
 
