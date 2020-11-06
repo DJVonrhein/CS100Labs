f01445c2bc47c0af7fa4053d8197a55885a120f6
DJVonrhein <dvonr001@ucr.edu>
2020-11-05 18:53:38 -0800

    First implementation of pow class, will test

---
+++ pow.hpp
@@ -0,0 +1,19 @@
+#ifndef __POW_HPP__
+#define __POW_HPP__
+
+#include "base.hpp"
+#include <cmath>
+
+class Sub : public Base {
+    public:
+        Sub(Base* l, Base* r) : Base() {lhs = l; rhs = r }
+        virtual double evaluate() {
+		return pow(lhs->evaluate(), rhs->evaluate()) 
+	}       
+	 virtual std::string stringify() { return (lhs->stringify() + " ** " + rhs->stringify()); }
+    private:
+        Base* lhs;
+        Base* rhs;
+};
+
+#endif
