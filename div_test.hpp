f4c2260e3f5f3b4267a534991acdb427d9f3c9c6
DJVonrhein <dvonr001@ucr.edu>
2020-11-05 19:42:32 -0800

    Fixed one test in div_test.hpp and implemented div class. div.hpp and div_test.hpp finalized.

--- div_test.hpp
+++ div_test.hpp
@@ -6,35 +6,34 @@
 #include "op.hpp"
 #include "div.hpp"
 
-TEST(DivTest, EvalDivBothNeg)
+TEST(DivTest, EvalDivBothNeg){
 	Op* lhs = new Op(-2);
 	Op* rhs = new Op(-4);
 	Div* test = new Div(lhs, rhs);
 	EXPECT_DOUBLE_EQ(test->evaluate(), 0.5);
 }
 
-TEST(DivTest, EvalDivDouble)
+TEST(DivTest, EvalDivDouble){
         Op* lhs = new Op(3);
         Op* rhs = new Op(-4.5);
         Div* test = new Div(lhs, rhs);
-        EXPECT_DOUBLE_EQ(test->evaluate(), -0.66);
+        EXPECT_DOUBLE_EQ(test->evaluate(), -0.6666666666666666);
 }
-
-TEST(DivTest, EvalDivZeroBySomething)
+TEST(DivTest, EvalDivZeroBySomething){
         Op* lhs = new Op(0);
         Op* rhs = new Op(-4);
         Div* test = new Div(lhs, rhs);
         EXPECT_DOUBLE_EQ(test->evaluate(), 0);
 }
 
-TEST(DivTest, StringifyDivBothNeg)
+TEST(DivTest, StringifyDivBothNeg){
         Op* lhs = new Op(-2);
         Op* rhs = new Op(-4);
         Div* test = new Div(lhs, rhs);
         EXPECT_EQ(test->stringify(), "-2.000000 / -4.000000");
 }
 
-TEST(DivTest, StringifyDivByDouble)
+TEST(DivTest, StringifyDivByDouble){
         Op* lhs = new Op(0);
         Op* rhs = new Op(1.98);
         Div* test = new Div(lhs, rhs);
