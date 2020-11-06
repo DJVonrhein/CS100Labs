07c8945ebec10ab41516da66b3b79b5eaaa855cd
DJVonrhein <dvonr001@ucr.edu>
2020-11-05 22:43:04 -0800

    all of add class is fixed/tested.

--- add_test.hpp
+++ add_test.hpp
@@ -2,30 +2,41 @@
 #define __ADD_TEST_HPP__
 
 #include "gtest/gtest.h"
-
 #include "add.hpp"
 
-Test(AddTest, AddMockPositiveTest) {
-	Add* test = new Add(2, 3);
+TEST(AddTest, AddMockPositiveTest) {
+	Op* two = new Op(2);
+	Op* three = new Op(3);
+	Add* test = new Add(two, three);
 	EXPECT_EQ(test->evaluate(), 5);
 }
 
-Test(AddTest, AddMockNegativeTest) {
-	Add* test = new Add(-2, -3);
+TEST(AddTest, AddMockNegativeTest) {
+	Op* negThree = new Op(-3);
+	Op* negTwo = new Op(-2);
+	Add* test = new Add(negThree, negTwo);
 	EXPECT_EQ(test->evaluate(), -5);
 }
 
-Test(AddTest, AddMockOnePosOneNeg) {
-	Add* test = new Add(-5, 6);
+TEST(AddTest, AddMockOnePosOneNeg) {
+	Op* negFive = new Op(-5);
+	Op* six = new Op(6);
+	Add* test = new Add(negFive, six);
 	EXPECT_EQ(test->evaluate(), 1);
 }
 
-Test(AddTest, AddZeroAndNeg) {
-	Add* test = new Add(0, -3);
+TEST(AddTest, AddZeroAndNeg) {
+	Op* zero = new Op(0);
+	Op* negThree = new Op(-3);
+	Add* test = new Add(zero, negThree);
 	EXPECT_EQ(test->evaluate(), -3);
 }
 
-Test(AddTest, AddZeroAndPos) {
-	Add* test = new Add(3, 0);
+TEST(AddTest, AddZeroAndPos) {
+	Op* three = new Op(3);
+	Op* zero = new Op(0);
+	Add* test = new Add(three, zero);
 	EXPECT_EQ(test->evaluate(), 3);
 }
+
+#endif
